af9b8f70e528a5da23c12edaf60032ab0e06341f
Артем Асламов <aslamov@promsoft.ru>
2021-11-26 11:30:22 +0700

    1

--- main.py
+++ main.py
@@ -1,46 +1,86 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
 import uvicorn
 from transitions import Machine
 
 app = FastAPI()
 
 ids = {}
-info = {
-    'size': '',
-    'pay_method': ''
-}
+
 states = ['choice_size', 'choice_pay', 'check', 'bb']
 
 
 @app.get('/send_msg/{msg}/{id}')
-def send_msg(msg: str, _id: int) -> str:
+def send_msg(msg: str, _id: int) -> Response:
+    """
+    .jbdzvjbdzv
+    :param msg:
+    :param _id:
+    :return:
+    """
 
     class Offer(object):
         pass
 
     state = Offer()
+    size = len(ids.get(_id, '   '))
     speach = {
         'choice_size': 'Какую вы хотите пиццу? Большую или маленькую?',
         'choice_pay': 'Как вы будете оплачивать?',
-        'check': f"Вы хотите {ids[_id][1]['size']}, оплата - {ids[_id][1]['pay_method']}",
+        'check': f"Вы хотите {ids.get(_id, '   ')[size-2]} пиццу, оплата - {ids.get(_id, '   ')[size-1]}",
         'bb': 'Спасибо за заказ'
     }
 
     transitions = [
-        {'trigger': 'большая', 'source': 'choice_size', 'dest': 'choice_pay'},
-        {'trigger': 'маленькая', 'source': 'choice_size', 'dest': 'choice_pay'},
+        {'trigger': 'большую', 'source': 'choice_size', 'dest': 'choice_pay'},
+        {'trigger': 'маленькую', 'source': 'choice_size', 'dest': 'choice_pay'},
         {'trigger': 'наличкой', 'source': 'choice_pay', 'dest': 'check'},
         {'trigger': 'безнал', 'source': 'choice_pay', 'dest': 'check'},
         {'trigger': 'да', 'source': 'check', 'dest': 'bb'}
     ]
 
-    machine = Machine(state, states=states, transitions=transitions, initial='')
+    machine = Machine(state, states=states, transitions=transitions, initial='choice_size')
 
-    if id in ids:
-        st = ids[id][0]
-        state.trigger(st)
+    if _id in ids:
+        st = ids[_id][0]
+        if st.state == 'bb':
+            return Response('Вы уже заказали пиццу')
+        msg = msg.lower()
+        print(st.state)
+        st.trigger(msg)
+        print(st.state)
+        ids[_id][0] = st
+        ids[_id].append(msg)
+
+        size = len(ids.get(_id, '   '))
+        speach = {
+            'choice_size': 'Какую вы хотите пиццу? Большую или маленькую?',
+            'choice_pay': 'Как вы будете оплачивать?',
+            'check': f"Вы хотите {ids.get(_id, '   ')[size - 2]} пиццу, оплата - {ids.get(_id, '   ')[size - 1]}?",
+            'bb': 'Спасибо за заказ'
+        }
+
+        print(ids[_id])
+        return Response(speach[st.state])
+
+    else:
+
+        size = len(ids.get(_id, '   '))
+        speach = {
+            'choice_size': 'Какую вы хотите пиццу? Большую или маленькую?',
+            'choice_pay': 'Как вы будете оплачивать?',
+            'check': f"Вы хотите {ids.get(_id, '   ')[size - 2]} пиццу, оплата - {ids.get(_id, '   ')[size - 1]}",
+            'bb': 'Спасибо за заказ'
+        }
+
+        ids[_id] = ['', '', '']
+        ids[_id][0] = state
+        return Response(speach['choice_size'])
 
 
+def run():
+    uvicorn.run(app, host='127.0.0.1', port=9999)
 
 
-    return str
+if __name__ == '__main__':
+    run()
--- tg_bot.py
+++ tg_bot.py
@@ -9,8 +9,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def content_messages(message):
-    response = httpx.get(f'http://127.0.0.1:5000/send_msg/{message}')
+    response = httpx.get(f'http://127.0.0.1:9999/send_msg/{message}')
     bot.send_message(message.from_user.id, response.text)
 
 
-bot.infinity_polling()
+bot.poll()
